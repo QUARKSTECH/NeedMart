26ebb403910cd991a52926d7bf5c3a268e92140f
Mansoor Ahmed <mansooronly4u@gmail.com>
2018-11-18 23:52:59 +0530

    updated old project refrences and names to new.

--- NeedMart.API/Startup.cs
+++ NeedMart.API/Startup.cs
@@ -30,7 +30,7 @@ namespace NeedMart.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DatingApp")));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("NeedMart")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
