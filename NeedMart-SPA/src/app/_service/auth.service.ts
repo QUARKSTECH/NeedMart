c2c46b90603beceb03a31a40cc564154d92cd647
Mansoor Ahmed <mansooronly4u@gmail.com>
2018-11-18 21:01:12 +0530

    Implemented alertify, ngxbootstrap, jwthelper for token and fixed userdropdown.

--- NeedMart-SPA/src/app/_service/auth.service.ts
+++ NeedMart-SPA/src/app/_service/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseurl = 'http://localhost:5000/api/auth/';
+  baseurl  =  environment.apiUrl + 'auth/'; // 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient ) { }
   login(model: any) {
@@ -16,8 +20,19 @@ constructor(private http: HttpClient ) { }
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         })
       );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseurl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- NeedMart-SPA/src/app/register/register.component.ts
+++ NeedMart-SPA/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_service/auth.service';
+import { AlertifyService } from '../_service/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      this.alertify.success('Registration Successfull');
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+  cancel() {
+    this.alertify.message('Cancelled');
+  }
 }
